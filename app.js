9e3b1d4265ac72542691148e608c91a041d9673f
JustawayL <caromids@gmail.com>
2018-04-25 16:54:55 -0500

    Creacion del tablero de juego

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
+
+const blockSize = 70 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var rows =      [0,1,2,1];
 var columns =   [1,0,1,2];
 
 
-function drawMatrix(rows,columns,size){
+function drawMatrix(rows,columns){
     let list = Array.from(Array(columns.length).keys())
     for (let index = 0; index < rows.length; index++) {
         list.forEach(element => {
             context.strokeStyle="black";
             if (columns[element] == 1 && rows[index] == 1){
-                context.fillStyle="green";
+                context.fillStyle="red";
             } else if (columns[element] == 2 && rows[index] == 2){
                 context.fillStyle = "yellow"
             } else {
-                context.fillStyle="red";                
+                context.fillStyle="green";                
             }
-            context.strokeRect(size*element,index*size,size,size);  
-            context.fillRect(size*element,index*size,size,size);                                    
+            context.strokeRect(blockSize*element,index*blockSize,blockSize,blockSize);  
+            context.fillRect(blockSize*element,index*blockSize,blockSize,blockSize);                                    
         });
     
     }
